5aae8e6df2a84642aa884310273294949d81a205
blu <baron.lu@apcapitalinvestment.com>
2023-05-29 08:22:56 +0800

    add go-api

---
+++ run.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/sirupsen/logrus"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		// 开启调试模式
+		Prefork:               false,
+		DisableStartupMessage: true,
+	})
+	//file, err := os.OpenFile("app.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer file.Close()
+	// 日志记录中间件
+	app.Use(cors.New())
+	app.Use(logger.New(logger.Config{
+		Format:       "${time} ${method} ${path} - ${status} - ${body} - ${latency}\n",
+		TimeFormat:   "2006-01-02 15:04:05",
+		TimeZone:     "Local",
+		TimeInterval: 500 * time.Millisecond,
+		Output:       file,
+	}))
+	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer file.Close()
+	// 注册一个中间件
+	app.Use(func(c *fiber.Ctx) error {
+		// 输出请求信息
+		fmt.Printf("[%s] %s\n", c.Method(), c.Path())
+
+		// 继续执行后续的操作
+		return c.Next()
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+	log.Fatal(app.Listen(":3000"))
+}
